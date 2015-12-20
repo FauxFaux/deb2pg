93ce3af42ab676afe667f2d876e5af1cd880d258
Chris West (Faux) <git@goeswhere.com>
2015-12-20 21:24:38 +0000

    theoretical new pack code

--- pack_package.py
+++ pack_package.py
@@ -15,27 +15,38 @@ def eat(source_package, source_version):
     print(tree)
 
 
-def pack_package(pack_dir, source_package, source_version):
+def ensure_exists(path):
     try:
-        os.mkdir('tmp')
+        os.mkdir(path)
     except FileExistsError:
         pass
-    with tempfile.TemporaryDirectory(dir='tmp') as destdir:
+
+
+def pack_package(pack_dir, source_package, source_version):
+    tmp_dir = 'tmp'
+
+    ensure_exists(tmp_dir)
+
+    with tempfile.TemporaryDirectory(dir=tmp_dir) as destdir:
         ingest_package.fetch(source_package, source_version, destdir)
         pkgfolder = os.path.join(destdir, 'pkg')
         subprocess.check_call(['git', 'init'], cwd=pkgfolder)
         subprocess.check_call(['git', 'add', '-A'], cwd=pkgfolder)
         tree = subprocess.check_output(['git', 'write-tree'], cwd=pkgfolder).decode('utf-8')
 
-        # oh so ghetto; pack-objects is hard
-        subprocess.check_call(['git', '--objects', '--format=%H', '--', tree], cwd=pkgfolder)
-        pack_location = os.path.join(pkgfolder, '.git/objects/pack/')
-        packs = [x for x in os.listdir(pack_location) if x.endswith('.pack')]
-        if 1 != len(packs):
-            raise ValueError("wrong number of packs", packs)
-        pack = packs[0]
+        objects = subprocess.Popen(['git', 'rev-list', '--objects', '--format=%H', '--', tree],
+                                   cwd=pkgfolder, stdout=subprocess.PIPE)
 
-        os.renames(os.path.join(pack_location, pack), os.path.join(pack_dir, tree[0:2] + '/' + tree))
+        ensure_exists(tree[0:2])
+
+        with open(os.path.join(pack_dir, tree[0:2] + '/' + tree), 'wb') as f:
+            subprocess.check_call(['git', 'pack-objects', '--stdout'],
+                                  stdin=objects.stdout,
+                                  stdout=f)
+
+        if 0 != objects.wait():
+            raise Exception('pipefail running rev-list')
+
     return tree
 
 
