b3f808c0a3b08f950babefc5cfb17a68ae7aea84
Chris West (Faux) <git@goeswhere.com>
2017-02-17 22:49:57 +0000

    ah right, tar files can be sparse so you don't get an fd.. and a length and argh

--- explode.py
+++ explode.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
 
-import libarchive
+import subprocess
+import tarfile
 
 
-def dump(entry: libarchive.ArchiveEntry):
-    print(entry.filetype, entry.mode, entry.mtime, entry.name, entry.path, entry.pathname, entry.size, entry.linkname,
-          entry.linkpath, entry.strmode)
+# def dump(entry: libarchive.ArchiveEntry):
+#     print(entry.filetype, entry.mode, entry.mtime, entry.name, entry.path, entry.pathname, entry.size, entry.linkname,
+#           entry.linkpath, entry.strmode)
 
-def unpack(f: libarchive.read.ArchiveRead):
-    for entry in f:
-        dump(entry)
+
+def unpack(readable):
+    p = subprocess.Popen(['bsdtar', '-c', '-f', '-', '@-'], stdin=readable, stdout=subprocess.PIPE)
+    with tarfile.open(mode='r|', fileobj=p.stdout) as tar:  # type: tarfile.TarFile
+        for entry in tar:  # type: tarfile.TarInfo
+            print(entry)
+            if entry.isreg():
+                unpack(tar.extractfile(entry))
+
+    if 0 != p.wait():
+        raise Exception('bsdtar failed')
 
 
 def main(path):
-    with libarchive.file_reader(path) as f:
+    with open(path) as f:
         unpack(f)
 
 
