83de7d67ad6638d7fe4d06b9c37f1d74e9d90ce3
Chris West (Faux) <git@goeswhere.com>
2017-02-26 15:54:07 +0000

    move pack.sh back internally

--- explode.py
+++ explode.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import collections
+import hashlib
 import io
+import os
 import shutil
 import subprocess
 import tarfile
@@ -8,6 +10,14 @@ import tempfile
 from typing import List
 
 import magic
+
+# for types only; actually using hashlib
+try:
+    from Crypto.Hash import SHA256
+except ImportError:
+    pass
+
+OUTPUT_TO = os.path.join(os.getcwd(), 'packed')
 
 ARCHIVE_TYPES = {
     'application/java-archive',
@@ -25,6 +35,20 @@ MAGIC_MIME.load()
 
 ignored_mime_types = set()
 useless_miming = collections.Counter()
+
+
+class ReNameableTemporaryFile:
+    def __init__(self, create_in: str):
+        self.name = tempfile.mktemp(dir=create_in, prefix='.', suffix='.tmp~')
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        try:
+            os.unlink(self.name)
+        except FileNotFoundError:
+            pass
 
 
 def guess_can_extract(b: bytes) -> bool:
@@ -92,9 +116,13 @@ def guess_can_extract(b: bytes) -> bool:
     return False
 
 
+Entry = collections.namedtuple('Entry', ['name', 'size', 'mode', 'hash'])
+
+
 def unpack(fd: io.TextIOWrapper, path: List[str]):
     print('unpacking {}'.format(path))
     p = subprocess.Popen(['bsdtar', '-c', '-f', '-', '@-'], stdin=fd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    tree = []  # type: List[Entry]
     with tarfile.open(mode='r|', fileobj=p.stdout) as tar:  # type: tarfile.TarFile
         for entry in tar:  # type: tarfile.TarInfo
             if not entry.isreg():
@@ -105,12 +133,9 @@ def unpack(fd: io.TextIOWrapper, path: List[str]):
             # returns different types if non-regular, but we know it's regular
             r = tar.extractfile(entry)  # type: tarfile.ExFileObject
             if not guess_can_extract(r.peek(64)):
-                packer = subprocess.Popen(['./pack.sh'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-                shutil.copyfileobj(r, packer.stdin)
-                packer.stdin.close()
-                if 0 != packer.wait():
-                    raise Exception("packing failed")
-                hash = packer.stdout.readline()
+                digest = pack_into_temp_file(r)
+
+                tree.append(Entry(path + [entry.name], entry.size, entry.mode, digest))
                 continue
 
             with tempfile.TemporaryFile() as tmp:
@@ -118,9 +143,36 @@ def unpack(fd: io.TextIOWrapper, path: List[str]):
                 tmp.flush()
                 tmp.seek(0)
                 unpack(tmp, path + [entry.name])
+    print(tree)
 
     if 0 != p.wait():
         print('bsdtar failed')
+
+
+def pack_into_temp_file(source: io.BufferedReader):
+    with ReNameableTemporaryFile(create_in=OUTPUT_TO) as f:
+        packer = subprocess.Popen(['lz4', '-5q', '-', f.name],
+                                  stdin=subprocess.PIPE,
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE)
+        h = hashlib.sha256()  # type: SHA256.SHA256Hash
+
+        while True:
+            buf = source.read(16 * 1024)
+            if not buf:
+                break
+            packer.stdin.write(buf)
+            h.update(buf)
+
+        packer.stdin.close()
+        if 0 != packer.wait():
+            print(packer.stdout.readline())
+            print(packer.stderr.readline())
+            raise Exception("packing failed")
+
+        digest = h.hexdigest()
+        os.rename(f.name, os.path.join(OUTPUT_TO, digest))
+        return digest
 
 
 def main(path):
