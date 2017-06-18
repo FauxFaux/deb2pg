6080ff662922608e54c5d29b57093a326b71d2ac
Chris West (Faux) <git@goeswhere.com>
2017-06-18 20:30:09 +0100

    pipestatus detects actual failures, instead of dying

--- ingest.sh
+++ ingest.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -eu
-set -o pipefail
-
 
 for f in "$@"; do
     parts=$(echo $f | sed 's/_/ /; s/.orig.tar.*//')
     ~/code/contentin/target/release/ci-gen -h capnp $f \
      | ~faux/code/deb2pg/deb2pg-rs/target/release/deb2pg-rs ${parts}
+     success=${PIPESTATUS[@]}
+     if [ '0 0' != "${success}" ]; then
+        echo $f >> ~/failures.log
+     fi
 done
