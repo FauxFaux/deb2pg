adb1b2b54376d8b7553186a96ef6a330c725753e
Chris West (Faux) <git@goeswhere.com>
2017-07-05 19:05:31 +0100

    copy is faster, but it's obvious the resulting table is dumb

--- reindex/src/main.rs
+++ reindex/src/main.rs
@@ -9,6 +9,7 @@ use std::fs;
 use std::io::Read;
 use std::io::Seek;
 use std::io::SeekFrom;
+use std::fmt::Write;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,11 +17,13 @@ fn main() {
         "postgres://faux@%2Frun%2Fpostgresql",
         postgres::TlsMode::None).unwrap();
     let tran = conn.transaction().unwrap();
-    let stat = tran.prepare("INSERT INTO tri (pos, tri) VALUES ($1, $2)").unwrap();
+    let stat = tran.prepare("COPY tri FROM STDIN").unwrap();
 
     let mut fp = fs::File::open(&args[1]).unwrap();
     fp.seek(SeekFrom::Start(16)).unwrap();
+    let mut write = String::new();
     loop {
+        write.truncate(0);
         let pos = fp.seek(SeekFrom::Current(0)).unwrap();
         if let Some(mut entry) = catfight::read_record(&mut fp).unwrap() {
             // len is the compressed length, but better than zero
@@ -28,12 +31,20 @@ fn main() {
             lz4::Decoder::new(&mut entry.reader).unwrap().read_to_end(&mut buf).unwrap();
 
             for t in index::trigrams_full(&String::from_utf8_lossy(&buf)) {
-                stat.execute(&[&(pos as i64), &(t as i32)]).unwrap();
+//                stat.execute(&[&(pos as i64), &(t as i32)]).unwrap();
+                write!(write, "{}\t{}\n", pos, t).unwrap();
             }
 
             entry.complete().unwrap();
+
+            stat.copy_in(&[], &mut write.as_bytes()).unwrap();
         } else {
             break;
+        }
+
+        // Basically random, although does need to be mod 16.
+        if 0 == (pos % 16 * 1024) {
+            println!("{:0.2}", pos as f32 / 1e9 * 100.0f32);
         }
     }
 
