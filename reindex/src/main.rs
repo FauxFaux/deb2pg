92618cfd65baa18f609f0dea89c8ce0a8d3e4bd0
Chris West (Faux) <git@goeswhere.com>
2017-07-16 13:25:46 +0100

    names / comments

--- reindex/src/main.rs
+++ reindex/src/main.rs
@@ -18,34 +18,44 @@ use std::io::Write;
 
 use byteorder::{LittleEndian, WriteBytesExt};
 
-struct Idx {
-    pos: u32,
-    len: u32,
+type Pos = u32;
+type Tri = u32;
+type Count = u32;
+
+struct TempFileChunk {
+    num_tris: Count,
+    pos: Pos,
 }
 
-fn read_pack<R: Read + Seek>(mut fp: R) -> (Vec<Idx>, HashMap<u32, u32>, fs::File) {
+/// Consume a pack file, and generate a temporary file containing the trigrams for every entry.
+/// `temp` file contains no metadata, literally just a concatenation of the tris for the first pos,
+/// then for the second, ...
+/// The returned `temp_index` of chunks stores their length, and which chunk-relative 'pos' they refer to
+/// The `trigram_count` for every trigram is also recorded.
+fn read_pack<R: Read + Seek>(mut pack: R) -> (fs::File, Vec<TempFileChunk>, HashMap<Tri, Count>) {
     let mut pos = 16;
-    fp.seek(SeekFrom::Start(pos)).unwrap();
-    let mut idx = Vec::with_capacity(200_000);
+    pack.seek(SeekFrom::Start(pos)).unwrap();
+
     let mut temp = io::BufWriter::new(tempfile::tempfile().unwrap());
-    let mut seen: HashMap<u32, u32> = HashMap::with_capacity(64*64);
+    let mut temp_index = Vec::with_capacity(200_000);
+    let mut trigram_count: HashMap<Tri, u32> = HashMap::with_capacity(64*64);
 
     loop {
-        if let Some(mut entry) = catfight::read_record(&mut fp).unwrap() {
+        if let Some(mut entry) = catfight::read_record(&mut pack).unwrap() {
             // len is the compressed length, but better than zero
             let mut buf = Vec::with_capacity(entry.len as usize);
             lz4::Decoder::new(&mut entry.reader).unwrap().read_to_end(&mut buf).unwrap();
 
             let mut tris: Vec<u32> = index::trigrams_full(&String::from_utf8_lossy(&buf)).into_iter().collect();
-            idx.push(Idx {
+            temp_index.push(TempFileChunk {
                 pos: pos as u32,
-                len: tris.len() as u32,
+                num_tris: tris.len() as u32,
             });
 
             tris.sort();
 
             for t in tris {
-                *seen.entry(t).or_insert(0) += 1;
+                *trigram_count.entry(t).or_insert(0) += 1;
 
                 // TODO: this actually should be in platform endian
                 temp.write_u32::<LittleEndian>(t).unwrap();
@@ -57,18 +67,13 @@ fn read_pack<R: Read + Seek>(mut fp: R) -> (Vec<Idx>, HashMap<u32, u32>, fs::Fil
         } else {
             break;
         }
-
-        // Basically random, although does need to be mod 16.
-        if 0 == (pos % (16 * 1024)) {
-            println!("{:0.2}", pos as f32 / 1e9 * 100.0f32);
-        }
     }
 
     let mut temp = temp.into_inner().unwrap();
 
     temp.flush().unwrap();
 
-    (idx, seen, temp)
+    (temp, temp_index, trigram_count)
 }
 
 fn main() {
@@ -77,19 +82,19 @@ fn main() {
     let fp = io::BufReader::new(fs::File::open(&args[1]).unwrap());
     let mut out = io::BufWriter::new(fs::File::create(&args[2]).unwrap());
 
-    let (idx, seen, temp) = read_pack(fp);
+    let (temp, temp_index, trigram_count) = read_pack(fp);
 
     let map = memmap::Mmap::open(&temp, memmap::Protection::Read).unwrap();
 
     let whole = unsafe { std::slice::from_raw_parts((map.ptr()) as *const u32, map.len() / 4) };
 
-    println!("{} seen", seen.len());
+    println!("{} seen", trigram_count.len());
 
-    let mut seen: Vec<(u32, u32)> = seen.iter().map(|x| (*x.0, *x.1)).collect();
+    let mut trigram_count: Vec<(u32, u32)> = trigram_count.into_iter().map(|x| (x.0, x.1)).collect();
 
-    seen.sort();
+    trigram_count.sort();
 
-    let mut seen = seen.iter();
+    let mut seen = trigram_count.iter();
 
     loop {
         let mut block = 0usize;
@@ -119,8 +124,8 @@ fn main() {
 
         let mut run = 0usize;
 
-        for part in &idx {
-            let subslice = &whole[run..(run + (part.len as usize))];
+        for part in &temp_index {
+            let subslice = &whole[run..(run + (part.num_tris as usize))];
             let mut start = match subslice.binary_search(&min) {
                 Ok(idx) | Err(idx) => idx,
             };
@@ -145,7 +150,7 @@ fn main() {
                 }
             }
 
-            run += part.len as usize;
+            run += part.num_tris as usize;
         }
 
         assert_eq!(map.len(), run * 4);
